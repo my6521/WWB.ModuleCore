4672e413aa76d596f60e76f18e068210ab18c959
王 <22379099@qq.com>
2022-02-11 13:27:37 +0800

    初始化1.0.0

---
+++ src/WWB.ModuleCore/Extensions/ApplicationInitializationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using WWB.ModuleCore.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WWB.ModuleCore.Extensions
+{
+    public static class ApplicationInitializationExtensions
+    {
+        public static IApplicationBuilder GetApplicationBuilder(this ApplicationInitializationContext applicationInitializationContext)
+        {
+            return applicationInitializationContext.ServiceProvider.GetRequiredService<IObjectAccessor<IApplicationBuilder>>().Value;
+        }
+
+
+    }
+}
---
+++ src/WWB.ModuleCore/ObjectAccessor.cs
@@ -0,0 +1,22 @@
+﻿namespace WWB.ModuleCore
+{
+
+    public interface IObjectAccessor<TType>
+    {
+        TType Value { get; set; }
+    }
+
+    public class ObjectAccessor<TType> : IObjectAccessor<TType>
+    {
+        public ObjectAccessor(TType obj)
+        {
+            Value = obj;
+        }
+
+        public ObjectAccessor()
+        {
+        }
+
+        public TType Value { get; set; }
+    }
+}
